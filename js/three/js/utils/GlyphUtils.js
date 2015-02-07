52df7791d516783964cffbf8d8d0f91ddb25c22c
Gregg Tavares <github@greggman.com>
2015-02-06 17:57:20 -0800

    add dos and ansi to GlpyhUtils

--- js/three/js/utils/GlyphUtils.js
+++ js/three/js/utils/GlyphUtils.js
@@ -37,22 +37,59 @@
     THREE.GlyphUtils = {
 
         unicodeRanges: {
-            ascii:              { start:     33, end:    127 },
-            greek:              { start:  0x370, end:  0x3FF },
-            tamil:              { start:  0xB80, end:  0xBFF },
-            telugu:             { start:  0xC00, end:  0xC7F },
-            runic:              { start: 0x16A0, end: 0x16FF },
-            canadian:           { start: 0x1400, end: 0x167F },
-            phoneticExtensions: { start: 0x1D00, end: 0x1DfF },
-            mathOps:            { start: 0x2200, end: 0x22FF },
-            miscTech:           { start: 0x2300, end: 0x23FF },
-            boxDrawing:         { start: 0x2500, end: 0x257F },
-            blockElements:      { start: 0x2580, end: 0x259F },
-            geometricShapes:    { start: 0x25A0, end: 0x25FF },
-            miscShapes:         { start: 0x2600, end: 0x26FF },
-            dingbats:           { start: 0x2700, end: 0x27BF },
-            cjk:                { start: 0x4E00, end: 0x5200 }, // really 0x9FCC!
+            ascii:              [ { start:     33, end:    127 } ],
+            ansi:               [ { start:   0x21, end:   0x7F },
+                                  { start:   0xA0, end:   0xFE },
+                                  { codePoints: [
+                                      0x20AC, 0x201A, 0x0192, 0x201E, 0x2026, 0x2020, 0x2021, 0x02C6,
+                                      0x2030, 0x0160, 0x2039, 0x0152, 0x017D, 0x2018, 0x2019, 0x201C,
+                                      0x201D, 0x2022, 0x2013, 0x2014, 0x02DC, 0x2122, 0x0161, 0x203A,
+                                      0x0153, 0x017E, 0x0178,
+                                    ]
+                                  } ],
+            dos:                [ { start:   0x21, end:   0x7E },
+                                  { codePoints: [
+                                      0x263A, 0x263B, 0x2665, 0x2666, 0x2663, 0x2660, 0x2022, 0x25D8,
+                                      0x25CB, 0x25D9, 0x2642, 0x2640, 0x266A, 0x266B, 0x263C, 0x25BA,
+                                      0x25C4, 0x2195, 0x203C, 0x00B6, 0x00A7, 0x25AC, 0x21A8, 0x2191,
+                                      0x2193, 0x2192, 0x2190, 0x221F, 0x2194, 0x25B2, 0x25BC, 0x2302,
+                                      0x00C7, 0x00FC, 0x00E9, 0x00E2, 0x00E4, 0x00E0, 0x00E5, 0x00E7,
+                                      0x00EA, 0x00EB, 0x00E8, 0x00EF, 0x00EE, 0x00EC, 0x00C4, 0x00C5,
+                                      0x00C9, 0x00E6, 0x00C6, 0x00F4, 0x00F6, 0x00F2, 0x00FB, 0x00F9,
+                                      0x00FF, 0x00D6, 0x00DC, 0x00A2, 0x00A3, 0x00A5, 0x20A7, 0x0192,
+                                      0x00E1, 0x00ED, 0x00F3, 0x00FA, 0x00F1, 0x00D1, 0x00AA, 0x00BA,
+                                      0x00BF, 0x2310, 0x00AC, 0x00BD, 0x00BC, 0x00A1, 0x00AB, 0x00BB,
+                                      0x2591, 0x2592, 0x2593, 0x2502, 0x2524, 0x2561, 0x2562, 0x2556,
+                                      0x2555, 0x2563, 0x2551, 0x2557, 0x255D, 0x255C, 0x255B, 0x2510,
+                                      0x2514, 0x2534, 0x252C, 0x251C, 0x2500, 0x253C, 0x255E, 0x255F,
+                                      0x255A, 0x2554, 0x2569, 0x2566, 0x2560, 0x2550, 0x256C, 0x2567,
+                                      0x2568, 0x2564, 0x2565, 0x2559, 0x2558, 0x2552, 0x2553, 0x256B,
+                                      0x256A, 0x2518, 0x250C, 0x2588, 0x2584, 0x258C, 0x2590, 0x2580,
+                                      0x03B1, 0x00DF, 0x0393, 0x03C0, 0x03A3, 0x03C3, 0x00B5, 0x03C4,
+                                      0x03A6, 0x0398, 0x03A9, 0x03B4, 0x221E, 0x03C6, 0x03B5, 0x2229,
+                                      0x2261, 0x00B1, 0x2265, 0x2264, 0x2320, 0x2321, 0x00F7, 0x2248,
+                                      0x00B0, 0x2219, 0x00B7, 0x221A, 0x207F, 0x00B2, 0x25A0, 0x00A0
+                                    ]
+                                  } ],
+            greek:              [ { start:  0x370, end:  0x3FF } ],
+            tamil:              [ { start:  0xB80, end:  0xBFF } ],
+            telugu:             [ { start:  0xC00, end:  0xC7F } ],
+            runic:              [ { start: 0x16A0, end: 0x16FF } ],
+            canadian:           [ { start: 0x1400, end: 0x167F } ],
+            phoneticExtensions: [ { start: 0x1D00, end: 0x1DfF } ],
+            mathOps:            [ { start: 0x2200, end: 0x22FF } ],
+            miscTech:           [ { start: 0x2300, end: 0x23FF } ],
+            boxDrawing:         [ { start: 0x2500, end: 0x257F } ],
+            blockElements:      [ { start: 0x2580, end: 0x259F } ],
+            geometricShapes:    [ { start: 0x25A0, end: 0x25FF } ],
+            miscShapes:         [ { start: 0x2600, end: 0x26FF } ],
+            dingbats:           [ { start: 0x2700, end: 0x27BF } ],
+            cjk:                [ { start: 0x4E00, end: 0x5200 } ], // really 0x9FCC!
         },
+
+
+
+
 
         makeGlyphShadingCanvas: (function(options, glyphsInfo) {
             glyphsInfo = glyphsInfo || {};
@@ -81,9 +118,21 @@
                 return s;
             }
 
+            function generateGlyphsFromCodePoints(codePoints) {
+                return codePoints.map(function(codePoint) {
+                    return String.fromCharCode(codePoint);
+                }).join("");
+            }
+
             function generate(rangeName) {
-                var range = THREE.GlyphUtils.unicodeRanges[rangeName];
-                return generateGlyphRange(range.start, range.end);
+                var ranges = THREE.GlyphUtils.unicodeRanges[rangeName];
+                return ranges.map(function(range) {
+                  if (range.codePoints) {
+                    return generateGlyphsFromCodePoints(range.codePoints);
+                  } else {
+                    return generateGlyphRange(range.start, range.end);
+                  }
+                }).join("");
             }
 
             var settings = copyOptions(defaults, options);
