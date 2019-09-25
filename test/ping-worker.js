c31472fa7eacc7b9fcefa7417033b4fb51b8b3f8
Gregg Tavares <github@greggman.com>
2019-09-25 20:54:02 +0900

    typo

--- test/ping-worker.js
+++ test/ping-worker.js
@@ -2,7 +2,7 @@ console.log('Started worker');
 
 self.postMessage('Hello');
 
-self.onMessage = (e) => {
-  self.postMessage(`from worker: ${e.data}`);
+self.onmessage = (e) => {
+  self.postMessage(`relayed from worker: ${e.data}`);
 };
 
