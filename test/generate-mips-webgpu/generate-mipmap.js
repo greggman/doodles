a02fba38d863a5e67fb46c278cec55c355083aee
Gregg Tavares <github@greggman.com>
2023-10-03 10:17:55 -0700

    after

--- test/generate-mips-webgpu/generate-mipmap.js
+++ test/generate-mips-webgpu/generate-mipmap.js
@@ -9,17 +9,55 @@ export function generateMipmap(device/*: GPUDevice*/, texture/*: GPUTexture*/) {
   }
   let {
     sampler,
+    uniformBuffer,
+    uniformValues,
   } = perDeviceInfo;
   const {
     pipelineByFormatAndView,
     moduleByView,
   } = perDeviceInfo;
-  const view = '2d';
+  const view = getViewDimensionForTexture(texture);
   let module = moduleByView[view];
   if (!module) {
+    let textureSnippet;
+    let sampleSnippet;
+    switch (view) {
+      case '2d':
+        textureSnippet = 'texture_2d<f32>';
+        sampleSnippet = 'textureSample(ourTexture, ourSampler, fsInput.texcoord)'
+        break;
+      case '2d-array':
+        textureSnippet = 'texture_2d_array<f32>';
+        sampleSnippet = `
+          textureSample(
+              ourTexture,
+              ourSampler,
+              fsInput.texcoord,
+              uni.layer)`;
+        break;
+      case 'cube':
+        textureSnippet = 'texture_cube<f32>';
+        sampleSnippet = `
+          textureSample(
+              ourTexture,
+              ourSampler,
+              faceMat[uni.layer] * vec3(fract(fsInput.texcoord), 1))`;
+        break;
+      default:
+        throw new Error(`unsupported view: ${type}`)
+    }
+
     module = device.createShaderModule({
       label: `mip level generation`,
       code: `
+        const faceMat = array(
+          mat3x3f( 0,  0,  -2,  0, -2,   0,  1,  1,   1),   // posx
+          mat3x3f( 0,  0,   2,  0, -2,   0, -1,  1,  -1),   // negx
+          mat3x3f( 2,  0,   0,  0,  0,   2, -1,  1,  -1),   // posy
+          mat3x3f( 2,  0,   0,  0,  0,  -2, -1, -1,   1),   // negy
+          mat3x3f( 2,  0,   0,  0, -2,   0, -1,  1,   1),   // posz
+          mat3x3f(-2,  0,   0,  0, -2,   0,  1,  1,  -1));  // negz
+
         struct VSOutput {
           @builtin(position) position: vec4f,
           @location(0) texcoord: vec2f,
@@ -41,12 +79,17 @@ export function generateMipmap(device/*: GPUDevice*/, texture/*: GPUTexture*/) {
           return vsOutput;
         }
 
+        struct Uniforms {
+          layer: u32,
+        };
+
         @group(0) @binding(0) var ourSampler: sampler;
-        @group(0) @binding(1) var ourTexture: texture_2d<f32>;
+        @group(0) @binding(1) var ourTexture: ${textureSnippet};
         @group(0) @binding(2) var<uniform> uni: Uniforms;
 
         @fragment fn fs(fsInput: VSOutput) -> @location(0) vec4f {
-          return textureSample(ourTexture, ourSampler, fsInput.texcoord);
+          _ = uni.layer; // make sure this is used so all pipelines have the same bindings
+          return ${sampleSnippet};
         }
       `,
     });
@@ -58,7 +101,12 @@ export function generateMipmap(device/*: GPUDevice*/, texture/*: GPUTexture*/) {
       minFilter: 'linear',
       magFilter: 'linear',
     });
-    Object.assign(perDeviceInfo, { sampler });
+    uniformBuffer = device.createBuffer({
+      size: 16,
+      usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    });
+    uniformValues = new Uint32Array(1);
+    Object.assign(perDeviceInfo, { sampler, uniformBuffer, uniformValues });
   }
 
 
@@ -81,8 +129,12 @@ export function generateMipmap(device/*: GPUDevice*/, texture/*: GPUTexture*/) {
   }
   const pipeline = pipelineByFormatAndView[id];
 
+  const dimension = getViewDimensionForTexture(texture);
   for (let baseMipLevel = 1; baseMipLevel < texture.mipLevelCount; ++baseMipLevel) {
     for (let baseArrayLayer = 0; baseArrayLayer < texture.depthOrArrayLayers; ++baseArrayLayer) {
+      uniformValues[0] = baseArrayLayer;
+      device.queue.writeBuffer(uniformBuffer, 0, uniformValues);
+
       const bindGroup = device.createBindGroup({
         layout: pipeline.getBindGroupLayout(0),
         entries: [
@@ -90,13 +142,12 @@ export function generateMipmap(device/*: GPUDevice*/, texture/*: GPUTexture*/) {
           {
             binding: 1,
             resource: texture.createView({
-              dimension: '2d',
+              dimension,
               baseMipLevel: baseMipLevel - 1,
               mipLevelCount: 1,
-              baseArrayLayer,
-              arrayLayerCount: 1,
             }),
           },
+          { binding: 2, resource: { buffer: uniformBuffer }},
         ],
       });
 
